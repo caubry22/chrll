1ee0e98296b5cada96665eadff67edd75f13c48a
Chloe AUBRY <caubry@e2r12p22.42.fr>
2020-10-01 19:41:55 +0200

    Shell

---
+++ Shell01/ex08/add_chelou.sh
@@ -0,0 +1,1 @@
+echo "obase=13; $(echo `echo "ibase=5;$(echo $FT_NBR1 | sed 'y/'\''\\"?!/01234/')" | bc`+`echo "ibase=5;$(echo $FT_NBR2 | tr 'mrdoc' '01234')" | bc` )" | bc | tr '0123456789ABC' 'gtaio luSnemf'
